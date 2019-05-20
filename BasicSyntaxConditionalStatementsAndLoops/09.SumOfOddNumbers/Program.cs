6955b63288fa84c35d49c81f0d347d989e844ba1
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-20 16:35:47 +0300

    Add sum-of-odd-numbers

--- BasicSyntaxConditionalStatementsAndLoops/09.SumOfOddNumbers/Program.cs
+++ BasicSyntaxConditionalStatementsAndLoops/09.SumOfOddNumbers/Program.cs
@@ -6,7 +6,18 @@ namespace _09.SumOfOddNumbers
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int number = int.Parse(Console.ReadLine());
+			int sum = 0;
+			int counter = 1;
+			while (number > 0)
+			{
+				Console.WriteLine(counter);
+				sum += counter;
+				counter += 2;
+				number--;
+			}
+
+			Console.WriteLine($"Sum: {sum}");
 		}
 	}
 }
