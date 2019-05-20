2bc94ec36aefb94d9953a01e04e13d09e3b32ca7
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-20 16:02:47 +0300

    Add back-in-30-minutes

--- BasicSyntaxConditionalStatementsAndLoops/04.BackIn30Minutes/Program.cs
+++ BasicSyntaxConditionalStatementsAndLoops/04.BackIn30Minutes/Program.cs
@@ -6,7 +6,28 @@ namespace _04.BackIn30Minutes
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int hours = int.Parse(Console.ReadLine());
+			int minutes = int.Parse(Console.ReadLine());
+			int newMinutes = minutes + 30;
+			while (newMinutes > 60)
+			{
+				newMinutes -= 60;
+				hours += 1;
+			}
+
+			while (hours >= 24)
+			{
+				hours = hours - 24;
+			}
+
+			if (newMinutes < 10)
+			{
+				Console.WriteLine($"{hours}:0{newMinutes}");
+			}
+			else
+			{
+				Console.WriteLine($"{hours}:{newMinutes}");
+			}
 		}
 	}
 }
