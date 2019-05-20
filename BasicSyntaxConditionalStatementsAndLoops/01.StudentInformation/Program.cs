37863574d05dcbf64d6d632775ffce838d37352d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-20 15:46:51 +0300

    Add student-information

--- BasicSyntaxConditionalStatementsAndLoops/01.StudentInformation/Program.cs
+++ BasicSyntaxConditionalStatementsAndLoops/01.StudentInformation/Program.cs
@@ -6,7 +6,10 @@ namespace _01.StudentInformation
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string name = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+			double grade = double.Parse(Console.ReadLine());
+			Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
 		}
 	}
 }
