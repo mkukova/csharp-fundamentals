74bbc4c9975ae76aa84f627df11f04777fc416c5
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-21 10:06:00 +0300

    Add division

--- BasicSyntaxConditionalStatementsAndLoopsExercise/02.Division/Program.cs
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/02.Division/Program.cs
@@ -6,7 +6,42 @@ namespace _02.Division
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int number = int.Parse(Console.ReadLine());
+			int divisible = 0;
+			bool isdivisible = true;
+			if (number % 10 == 0)
+			{
+				divisible = 10;
+			}
+			else if (number % 7 == 0)
+			{
+				divisible = 7;
+			}
+			else if (number % 6 == 0)
+			{
+				divisible = 6;
+			}
+			else if (number % 3 == 0)
+			{
+				divisible = 3;
+			}
+			else if (number % 2 == 0)
+			{
+				divisible = 2;
+			}
+			else
+			{
+				isdivisible = false;
+			}
+
+			if (isdivisible)
+			{
+				Console.WriteLine($"The number is divisible by {divisible}");
+			}
+			else
+			{
+				Console.WriteLine("Not divisible");
+			}
 		}
 	}
 }
