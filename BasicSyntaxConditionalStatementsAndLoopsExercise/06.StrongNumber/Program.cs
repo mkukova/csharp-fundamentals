239a9c09f2f2c3e537a9be873f304a4d0c5e088d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-21 11:53:11 +0300

    Add strong-number

--- BasicSyntaxConditionalStatementsAndLoopsExercise/06.StrongNumber/Program.cs
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/06.StrongNumber/Program.cs
@@ -6,7 +6,31 @@ namespace _06.StrongNumber
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int number = int.Parse(Console.ReadLine());
+			int sum = 0;
+			int digit = 0;
+			int number1 = number;
+			while (number>0)
+			{
+				digit = number % 10;
+				int multiplication = 1;
+				for (int i=1; i<=digit; i++)
+				{
+					multiplication = i * multiplication;
+				}
+
+				sum += multiplication;
+				number = number / 10;
+			}
+
+			 if(sum==number1)
+			{
+				Console.WriteLine("yes");
+			}
+			 else
+			{
+				Console.WriteLine("no");
+			}
 		}
 	}
 }
