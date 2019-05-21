05fbffeca7f5296132c1fcf18a8577b3ab9119aa
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-21 11:38:12 +0300

    Add login

--- BasicSyntaxConditionalStatementsAndLoopsExercise/05.Login/Program.cs
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/05.Login/Program.cs
@@ -6,7 +6,49 @@ namespace _05.Login
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string username = Console.ReadLine();
+			int counter = 0;
+			bool isValidLetter = false;
+			int j = 0;
+			while (counter < 4)
+			{
+				string password = Console.ReadLine();
+				for (int i = username.Length - 1; i >= 0; i--)
+				{
+					if (username[i] == password[j])
+					{
+						j++;
+						isValidLetter = true;
+						continue;
+					}
+					else
+					{
+						isValidLetter = false;
+						break;
+					}
+				}
+
+				if (isValidLetter)
+				{
+					Console.WriteLine($"User {username} logged in.");
+					return;
+				}
+				else
+				{
+					counter++;
+					if (counter == 4)
+					{
+						Console.WriteLine($"User {username} blocked!");
+					}
+					else
+					{
+						Console.WriteLine("Incorrect password. Try again.");
+					}
+				}
+
+				j = 0;
+				isValidLetter = false;
+			}
 		}
 	}
 }
