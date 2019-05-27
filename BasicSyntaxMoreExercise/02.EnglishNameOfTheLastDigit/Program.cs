902c53edc90819d6b825fff9d13ad09ca420e670
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-27 13:54:37 +0300

    Add English Name Of The Last Digit

--- BasicSyntaxMoreExercise/02.EnglishNameOfTheLastDigit/Program.cs
+++ BasicSyntaxMoreExercise/02.EnglishNameOfTheLastDigit/Program.cs
@@ -6,7 +6,41 @@ namespace _02.EnglishNameOfTheLastDigit
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int number = int.Parse(Console.ReadLine());
+			int digit = number % 10;
+			switch (digit)
+			{
+				case 1:
+					Console.WriteLine("one");
+					break;
+				case 2:
+					Console.WriteLine("two");
+					break;
+				case 3:
+					Console.WriteLine("tree");
+					break;
+				case 4:
+					Console.WriteLine("four");
+					break;
+				case 5:
+					Console.WriteLine("five");
+					break;
+				case 6:
+					Console.WriteLine("six");
+					break;
+				case 7:
+					Console.WriteLine("seven");
+					break;
+				case 8:
+					Console.WriteLine("eight");
+					break;
+				case 9:
+					Console.WriteLine("nine");
+					break;
+				case 0:
+					Console.WriteLine("zero");
+					break;
+			}
 		}
 	}
 }
