3f87ebb0d1e0dd07c06f2d0208f9a779e0a1a1c5
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-27 11:50:51 +0300

    Add rage expenses

--- BasicSyntaxConditionalStatementsAndLoopsExercise/10.RageExpenses/Program.cs
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/10.RageExpenses/Program.cs
@@ -6,7 +6,20 @@ namespace _10.RageExpenses
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int lostGames = int.Parse(Console.ReadLine());
+			double headsetPrice = double.Parse(Console.ReadLine());
+			double mousePrice = double.Parse(Console.ReadLine());
+			double keyboardPrice = double.Parse(Console.ReadLine());
+			double displayPrice = double.Parse(Console.ReadLine());
+			int trashedHeadset = lostGames / 2;
+			int trashedMouse = lostGames / 3;
+			int trashedKeyboard = lostGames / 6;
+			int trashedDisplay = trashedKeyboard / 2;
+			double expenses = headsetPrice * trashedHeadset;
+			expenses += mousePrice * trashedMouse;
+			expenses += keyboardPrice * trashedKeyboard;
+			expenses += displayPrice * trashedDisplay;
+			Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
 		}
 	}
 }
