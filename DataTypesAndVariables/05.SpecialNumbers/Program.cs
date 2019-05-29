fa91e5fc75c0fe3e8e485bf35880ed7e749523f2
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-29 15:45:32 +0300

    Add special numbers

--- DataTypesAndVariables/05.SpecialNumbers/Program.cs
+++ DataTypesAndVariables/05.SpecialNumbers/Program.cs
@@ -6,7 +6,36 @@ namespace _05.SpecialNumbers
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int counter = int.Parse(Console.ReadLine());
+			bool isSpecial = false;
+			for (int i = 1; i <= counter; i++)
+			{
+				int number = i;
+				int sum = 0;
+				while (number > 0)
+				{
+					sum += number % 10;
+					number = number / 10;
+				}
+
+				if (sum == 5 || sum == 7 || sum == 11)
+				{
+					isSpecial = true;
+				}
+				else
+				{
+					isSpecial = false;
+				}
+
+				if (isSpecial)
+				{
+					Console.WriteLine($"{i} -> True");
+				}
+				else
+				{
+					Console.WriteLine($"{i} -> False");
+				}
+			}
 		}
 	}
 }
