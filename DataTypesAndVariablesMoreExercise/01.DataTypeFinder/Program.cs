fb125f72962404f919315a44a74152e5087a489b
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-31 12:18:38 +0300

    Add data-type-finder

--- DataTypesAndVariablesMoreExercise/01.DataTypeFinder/Program.cs
+++ DataTypesAndVariablesMoreExercise/01.DataTypeFinder/Program.cs
@@ -6,7 +6,36 @@ namespace _01.DataTypeFinder
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string input = Console.ReadLine();
+			int valueInt;
+			double valueDouble;
+			char valueChar;
+			bool valueBoolean;
+			while (input != "END")
+			{
+				if (int.TryParse(input, out valueInt))
+				{
+					Console.WriteLine($"{input} is integer type");
+				}
+				else if (double.TryParse(input, out valueDouble))
+				{
+					Console.WriteLine($"{input} is floating point type");
+				}
+				else if (char.TryParse(input, out valueChar))
+				{
+					Console.WriteLine($"{input} is character type");
+				}
+				else if (bool.TryParse(input, out valueBoolean))
+				{
+					Console.WriteLine($"{input} is boolean type");
+				}
+				else
+				{
+					Console.WriteLine($"{input} is string type");
+				}
+
+				input = Console.ReadLine();
+			}
 		}
 	}
 }
