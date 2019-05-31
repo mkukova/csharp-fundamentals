547d0f83eaaf35505cc46cb6642d1b9ed7998df8
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-31 11:34:00 +0300

    Add snowballs

--- DataTypesAndVariablesExercise/11.Snowballs/Program.cs
+++ DataTypesAndVariablesExercise/11.Snowballs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace _11.Snowballs
 {
@@ -6,7 +7,29 @@ namespace _11.Snowballs
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int snowballCount = int.Parse(Console.ReadLine());
+			BigInteger maxValue = 0; 
+			int bestSnow = 0;
+			int bestTime = 0;
+			int bestQuality = 0;
+			while (snowballCount > 0)
+			{
+				int snowballSnow = int.Parse(Console.ReadLine());
+				int snowballTime = int.Parse(Console.ReadLine());
+				int snowballQuality = int.Parse(Console.ReadLine()); 
+				BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+				if (snowballValue > maxValue)
+				{
+					maxValue = snowballValue;
+					bestSnow = snowballSnow;
+					bestTime = snowballTime;
+					bestQuality = snowballQuality;
+				}
+
+				snowballCount--;
+			}
+
+			Console.WriteLine($"{bestSnow} : {bestTime} = {maxValue} ({bestQuality})");
 		}
 	}
 }
