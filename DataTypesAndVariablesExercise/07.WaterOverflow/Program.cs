37adc38b50ee6b4d281e5ad1df7de34c10878e20
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-31 10:13:53 +0300

    Add water-overflow

--- DataTypesAndVariablesExercise/07.WaterOverflow/Program.cs
+++ DataTypesAndVariablesExercise/07.WaterOverflow/Program.cs
@@ -6,7 +6,20 @@ namespace _07.WaterOverflow
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int numberLines = int.Parse(Console.ReadLine());
+			int sum = 0;
+			for (int i = 0; i < numberLines; i++)
+			{
+				int liters = int.Parse(Console.ReadLine());
+				sum += liters;
+				if (sum > 255)
+				{
+					sum -= liters;
+					Console.WriteLine("Insufficient capacity!");
+				}
+			}
+
+			Console.WriteLine(sum);
 		}
 	}
 }
