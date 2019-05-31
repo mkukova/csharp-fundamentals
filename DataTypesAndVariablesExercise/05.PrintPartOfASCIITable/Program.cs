a90743899537695d5134b98814782f7922faeee8
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-31 10:01:38 +0300

    Add print-part-of-ASCII-table

--- DataTypesAndVariablesExercise/05.PrintPartOfASCIITable/Program.cs
+++ DataTypesAndVariablesExercise/05.PrintPartOfASCIITable/Program.cs
@@ -6,7 +6,12 @@ namespace _05.PrintPartOfASCIITable
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int startIndex = int.Parse(Console.ReadLine());
+			int finalindex = int.Parse(Console.ReadLine());
+			for (int i = startIndex; i <= finalindex; i++)
+			{
+				Console.Write($"{(char)i} ");
+			}
 		}
 	}
 }
