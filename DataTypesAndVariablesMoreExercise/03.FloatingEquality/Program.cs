59864e286984970d7899e141042e1ce85a4ec3f9
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-31 13:14:41 +0300

    Add floating-equality

--- DataTypesAndVariablesMoreExercise/03.FloatingEquality/Program.cs
+++ DataTypesAndVariablesMoreExercise/03.FloatingEquality/Program.cs
@@ -6,7 +6,18 @@ namespace _03.FloatingEquality
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			double firstNumber = double.Parse(Console.ReadLine());
+			double secondNumber = double.Parse(Console.ReadLine());
+			double eps = 0.000001;
+			double difference = Math.Abs(firstNumber - secondNumber);
+			if (difference >= eps)
+			{
+				Console.WriteLine("False");
+			}
+			else
+			{
+				Console.WriteLine("True");
+			}
 		}
 	}
 }
