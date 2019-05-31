d5e4d4ac1b96471d079c73a047f3b7a088becb9d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-31 13:56:43 +0300

    Add balanced-brackets

--- DataTypesAndVariablesMoreExercise/06.BalancedBrackets/Program.cs
+++ DataTypesAndVariablesMoreExercise/06.BalancedBrackets/Program.cs
@@ -6,7 +6,37 @@ namespace _06.BalancedBrackets
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = int.Parse(Console.ReadLine());
+			int openCount = 0;
+			int closeCount = 0;
+
+			for (int i = 1; i <= n; i++)
+			{
+				string input = Console.ReadLine();
+				if (input == "(")
+				{
+					openCount++;
+
+				}
+				else if (input == ")")
+				{
+					closeCount++;
+					if (openCount - closeCount != 0)
+					{
+						Console.WriteLine("UNBALANCED");
+						return;
+					}
+				}
+			}
+
+			if (openCount == closeCount)
+			{
+				Console.WriteLine("BALANCED");
+			}
+			else
+			{
+				Console.WriteLine("UNBALANCED");
+			}
 		}
 	}
 }
