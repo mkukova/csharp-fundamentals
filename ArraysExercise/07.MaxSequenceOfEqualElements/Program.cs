4dd809848aa2a3be93e082e508be45309b02054e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-03 15:21:55 +0300

    Add max-sequence-of-equal-elements

--- ArraysExercise/07.MaxSequenceOfEqualElements/Program.cs
+++ ArraysExercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _07.MaxSequenceOfEqualElements
 {
@@ -6,7 +7,40 @@ namespace _07.MaxSequenceOfEqualElements
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] numbers = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			int currentCount = 1;
+			int maxCount = 1;
+			int number = numbers[0];
+
+			for (int i = 1; i < numbers.Length; i++)
+			{
+				int previousNumber = numbers[i - 1];
+				int currentNumber = numbers[i];
+				if (previousNumber == currentNumber)
+				{
+					currentCount++;
+					if (currentCount > maxCount)
+					{
+						maxCount = currentCount;
+						number = currentNumber;
+					}
+				}
+				else
+				{
+					currentCount = 1;
+				}
+			}
+
+			for (int i = 0; i < maxCount; i++)
+			{
+				Console.Write(number + " ");
+			}
+
+			Console.WriteLine();
 		}
 	}
 }
