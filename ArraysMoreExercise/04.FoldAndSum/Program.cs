e09f006dda9a7ca64c0c04a7166a471f98d05684
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-05 16:52:00 +0300

    Add fold-and-sum

--- ArraysMoreExercise/04.FoldAndSum/Program.cs
+++ ArraysMoreExercise/04.FoldAndSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04.FoldAndSum
 {
@@ -6,7 +7,24 @@ namespace _04.FoldAndSum
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			int[] leftFold = new int[inputArray.Length / 4];
+			int[] rightFold = new int[inputArray.Length / 4];
+			int[] resultArray = new int[inputArray.Length / 2];
+
+			for (int i = 0; i < (inputArray.Length / 4); i++)
+			{
+				leftFold[i] = inputArray[(inputArray.Length / 4) - 1 - i];
+				rightFold[i] = inputArray[inputArray.Length - 1 - i];
+			}
+
+			for (int i = 0; i < (inputArray.Length / 4); i++)
+			{
+				resultArray[i] = leftFold[i] + inputArray[inputArray.Length / 4 + i];
+				resultArray[inputArray.Length / 4 + i] = rightFold[i] + inputArray[inputArray.Length / 2 + i];
+			}
+
+			Console.WriteLine(string.Join(" ", resultArray));
 		}
 	}
 }
