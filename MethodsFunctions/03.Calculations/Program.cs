e61df118e25a40756d86127c55984c605df245cb
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-10 14:30:43 +0300

    Add calculations

--- MethodsFunctions/03.Calculations/Program.cs
+++ MethodsFunctions/03.Calculations/Program.cs
@@ -6,7 +6,47 @@ namespace _03.Calculations
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string command = Console.ReadLine().ToLower();
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+			switch (command)
+			{
+				case "add":
+					Add(a, b);
+					break;
+				case "multiply":
+					Multiply(a, b);
+					break;
+				case "subtract":
+					Subtract(a, b);
+					break;
+				case "divide":
+					Divide(a, b);
+					break;
+				default:
+					Console.WriteLine("Error!");
+					break;
+			}
+		}
+
+		static void Add(int a, int b)
+		{
+			Console.WriteLine(a + b);
+		}
+
+		static void Multiply(int a, int b)
+		{
+			Console.WriteLine(a * b);
+		}
+
+		static void Divide(int a, int b)
+		{
+			Console.WriteLine(a / b);
+		}
+
+		static void Subtract(int a, int b)
+		{
+			Console.WriteLine(a - b);
 		}
 	}
 }
