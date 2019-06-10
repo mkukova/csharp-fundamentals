d8c70e23d17eb9089b6c42c67a8158809c995057
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-10 14:50:56 +0300

    Add orders

--- MethodsFunctions/05.Orders/Program.cs
+++ MethodsFunctions/05.Orders/Program.cs
@@ -6,7 +6,31 @@ namespace _05.Orders
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string product = Console.ReadLine().ToLower();
+			int quantity = int.Parse(Console.ReadLine());
+			OrderPrice(product, quantity);
+		}
+
+		static void OrderPrice(string product, int quantity)
+		{
+			switch (product)
+			{
+				case "coffee":
+					Console.WriteLine($"{(quantity * 1.5):F2}");
+					break;
+				case "water":
+					Console.WriteLine($"{(quantity * 1.0):F2}");
+					break;
+				case "coke":
+					Console.WriteLine($"{(quantity * 1.4):F2}");
+					break;
+				case "snacks":
+					Console.WriteLine($"{(quantity * 2.0):F2}");
+					break;
+				default:
+					Console.WriteLine("Wrong product name");
+					break;
+			}
 		}
 	}
 }
