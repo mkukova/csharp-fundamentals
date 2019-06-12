5f0398092860f0bb3cd3582dd433fc5d984b9fea
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-12 14:52:19 +0300

    Add vowels count

--- MethodsExercise/02.VowelsCount/Program.cs
+++ MethodsExercise/02.VowelsCount/Program.cs
@@ -6,7 +6,29 @@ namespace _02.VowelsCount
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string text = Console.ReadLine().ToLower();
+			int numberVowels = VowelsCount(text);
+			Console.WriteLine(numberVowels);
+		}
+
+		static int VowelsCount(string a)
+		{
+			int counter = 0;
+			for (int i = 0; i < a.Length; i++)
+			{
+				switch (a[i])
+				{
+					case 'o':
+					case 'i':
+					case 'a':
+					case 'u':
+					case 'e':
+						counter++;
+						break;
+				}
+			}
+
+			return counter;
 		}
 	}
 }
