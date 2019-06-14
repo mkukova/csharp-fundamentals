6a56c9f76db83c776b4d12a77050fcd2137c17a7
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-14 14:56:52 +0300

    Add NxN-matrix

--- MethodsExercise/07.NxNMatrix/Program.cs
+++ MethodsExercise/07.NxNMatrix/Program.cs
@@ -6,7 +6,21 @@ namespace _07.NxNMatrix
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int number = int.Parse(Console.ReadLine());
+			Matrix(number);
+		}
+
+		static void Matrix(int number)
+		{
+			for (int i = 0; i < number; i++)
+			{
+				for (int j = 0; j < number; j++)
+				{
+					Console.Write(number + " ");
+				}
+
+				Console.WriteLine();
+			}
 		}
 	}
 }
