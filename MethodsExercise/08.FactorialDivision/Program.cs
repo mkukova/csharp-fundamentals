3ff395740fe80044080a26261a3f38e72992735e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-14 15:16:41 +0300

    Add factorial-division

--- MethodsExercise/08.FactorialDivision/Program.cs
+++ MethodsExercise/08.FactorialDivision/Program.cs
@@ -6,7 +6,27 @@ namespace _08.FactorialDivision
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int number1 = int.Parse(Console.ReadLine());
+			int number2 = int.Parse(Console.ReadLine());
+			Console.WriteLine($"{FactorialDivision(number1, number2):F2}");
+		}
+
+		static long Factorial(int a)
+		{
+			long result = 1;
+			for (int i = 1; i <= a; i++)
+			{
+				result *= i;
+			}
+
+			return result;
+		}
+
+		static double FactorialDivision(int a, int b)
+		{
+			double result = 0;
+			result = 1.0 * Factorial(a) / Factorial(b);
+			return result;
 		}
 	}
 }
