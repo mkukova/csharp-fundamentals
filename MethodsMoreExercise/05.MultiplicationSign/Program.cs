8228491c4671135123d3b3ad93f770d8d36ecbb7
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-19 13:24:48 +0300

    Add multiplication- sign

--- MethodsMoreExercise/05.MultiplicationSign/Program.cs
+++ MethodsMoreExercise/05.MultiplicationSign/Program.cs
@@ -6,7 +6,45 @@ namespace _05.MultiplicationSign
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] nums = new int[3];
+
+			for (int i = 0; i < 3; i++)
+			{
+				nums[i] = int.Parse(Console.ReadLine());
+			}
+
+			MultiplicationSign(nums);
+		}
+
+		static void MultiplicationSign(int[] nums)
+		{
+			int isNegative = 0;
+
+			for (int i = 0; i < 3; i++)
+			{
+				if (nums[i] < 0)
+				{
+					isNegative++;
+				}
+			}
+
+			if (nums[0] == 0 || nums[1] == 0 || nums[2] == 0)
+			{
+				Console.WriteLine("zero");
+			}
+			else if (nums[0] > 0 && nums[1] > 0 && nums[2] > 0)
+			{
+				Console.WriteLine("positive");
+			}
+			else if (isNegative == 2)
+			{
+				Console.WriteLine("positive");
+			}
+			else
+			{
+				Console.WriteLine("negative");
+			}
+
 		}
 	}
 }
