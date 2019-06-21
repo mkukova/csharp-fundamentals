03f9a9ec847262097b32e6086362c8657edfb1af
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-21 14:28:58 +0300

    Add list-of-products

--- ListsArraysAdvanced/04.ListOfProducts/Program.cs
+++ ListsArraysAdvanced/04.ListOfProducts/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04.ListOfProducts
 {
@@ -6,7 +8,21 @@ namespace _04.ListOfProducts
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int numberProducts = int.Parse(Console.ReadLine());
+			List<string> products = new List<string>();
+
+			for (int i = 0; i < numberProducts; i++)
+			{
+				string currentProduct = Console.ReadLine();
+				products.Add(currentProduct);
+			}
+
+			products.Sort();
+
+			for (int i = 0; i < numberProducts; i++)
+			{
+				Console.WriteLine($"{i + 1}.{products[i]}");
+			}
 		}
 	}
 }
