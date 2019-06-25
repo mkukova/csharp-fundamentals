3efc1988e4d59ed9c2c0d96bc574c5fe38ed4b13
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-06-25 17:25:07 +0300

    Add bomb-numbers

--- ListsExercise/05.BombNumbers/Program.cs
+++ ListsExercise/05.BombNumbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.BombNumbers
 {
@@ -6,7 +8,32 @@ namespace _05.BombNumbers
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<int> numbers = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToList();
+
+			int[] specialNumbers = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+			int bombNum = specialNumbers[0];
+			int power = specialNumbers[1];
+
+			while (numbers.Contains(bombNum))
+			{
+				int indexBombNumber = numbers.IndexOf(bombNum);
+				for (int i = indexBombNumber - power; i <= indexBombNumber + power; i++)
+				{
+					if (i >= 0 && i < numbers.Count)
+					{
+						numbers[i] = 0;
+					}
+				}
+			}
+
+			int sum = numbers.Sum();
+			Console.WriteLine(sum);
 		}
 	}
 }
