8478692287352c2999f5177824f64388a139b67d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-01 14:36:41 +0300

    Add take-skip-rope

--- ListsMoreExercise/03.TakeSkipRope/Program.cs
+++ ListsMoreExercise/03.TakeSkipRope/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _03.TakeSkipRope
 {
@@ -6,7 +9,75 @@ namespace _03.TakeSkipRope
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string hiddenMessage = Console.ReadLine();
+			List<int> numbers = new List<int>();
+			List<int> takeList = new List<int>();
+			List<int> skipList = new List<int>();
+			List<string> nonNumbers = new List<string>();
+			string result = string.Empty;
+
+			for (int i = 0; i < hiddenMessage.Length; i++)
+			{
+				if (char.IsDigit(hiddenMessage[i]))
+				{
+					numbers.Add(int.Parse(hiddenMessage[i].ToString()));
+				}
+				else
+				{
+					nonNumbers.Add(hiddenMessage[i].ToString());
+				}
+			}
+
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				if (i % 2 == 0)
+				{
+					takeList.Add(numbers[i]);
+				}
+				else
+				{
+					skipList.Add(numbers[i]);
+				}
+			}
+
+			nonNumbers.Reverse();
+			int nonNumbersLenght = nonNumbers.Count - 1;
+			bool isEmpty = false;
+
+			for (int i = 0; i < takeList.Count; i++)
+			{
+				for (int j = 0; j < takeList[i]; j++)
+				{
+					result += nonNumbers[nonNumbersLenght];
+					nonNumbers.RemoveAt(nonNumbersLenght);
+					nonNumbersLenght--;
+
+					if (nonNumbersLenght < 0)
+					{
+						isEmpty = true;
+						break;
+					}
+				}
+
+				for (int k = 0; k < skipList[i]; k++)
+				{
+					nonNumbers.RemoveAt(nonNumbersLenght);
+					nonNumbersLenght--;
+
+					if (nonNumbersLenght < 0)
+					{
+						isEmpty = true;
+						break;
+					}
+				}
+
+				if (isEmpty)
+				{
+					break;
+				}
+			}
+
+			Console.WriteLine(result);
 		}
 	}
 }
