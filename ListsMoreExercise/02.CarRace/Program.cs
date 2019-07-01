0cb1ec765f5ea40bc4a72768179e2e6d229b6540
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-01 13:18:35 +0300

    Add car-race

--- ListsMoreExercise/02.CarRace/Program.cs
+++ ListsMoreExercise/02.CarRace/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _02.CarRace
 {
@@ -6,7 +8,45 @@ namespace _02.CarRace
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<int> numbers = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToList();
+
+			int countOfNumbers = numbers.Count;
+			double firstCarTime = 0;
+
+			for (int i = 0; i < countOfNumbers / 2; i++)
+			{
+				firstCarTime += numbers[i];
+
+				if (numbers[i] == 0)
+				{
+					firstCarTime = 0.8 * firstCarTime;
+				}
+			}
+
+			double secondCarTime = 0;
+
+			for (int i = countOfNumbers - 1; i > countOfNumbers / 2; i--)
+			{
+				secondCarTime += numbers[i];
+
+				if (numbers[i] == 0)
+				{
+					secondCarTime = 0.8 * secondCarTime;
+				}
+			}
+
+			if (firstCarTime < secondCarTime)
+			{
+				Console.WriteLine($"The winner is left with total time: {firstCarTime}");
+			}
+			else
+			{
+				Console.WriteLine($"The winner is right with total time: {secondCarTime}");
+			}
+
 		}
 	}
 }
