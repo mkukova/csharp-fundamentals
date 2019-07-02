aaa3c9283fbd90dc2e73e6a3db9f9ec02d536b1b
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-02 14:18:48 +0300

    Add mixed-up-lists

--- ListsMoreExercise/04.MixedUpLists/Program.cs
+++ ListsMoreExercise/04.MixedUpLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04.MixedUpLists
 {
@@ -6,7 +8,69 @@ namespace _04.MixedUpLists
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<int> firstLine = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToList();
+
+			List<int> secondLine = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToList();
+
+			List<int> midResult = new List<int>();
+			List<int> result = new List<int>();
+			int[] range = new int[2];
+
+			int firstLineLenght = firstLine.Count;
+			int secondLineLenght = secondLine.Count;
+
+			if (firstLineLenght > secondLineLenght)
+			{
+				secondLine.Reverse();
+
+				for (int i = 0; i < secondLineLenght; i++)
+				{
+					midResult.Add(firstLine[i]);
+					midResult.Add(secondLine[i]);
+				}
+
+				range[0] = firstLine[firstLineLenght - 1];
+				range[1] = firstLine[firstLineLenght - 2];
+			}
+			else
+			{
+				secondLine.Reverse();
+				range[0] = secondLine[secondLineLenght - 1];
+				range[1] = secondLine[secondLineLenght - 2];
+				secondLine.RemoveAt(secondLineLenght - 1);
+				secondLine.RemoveAt(secondLineLenght - 2);
+
+				for (int i = 0; i < firstLineLenght; i++)
+				{
+					midResult.Add(firstLine[i]);
+					midResult.Add(secondLine[i]);
+				}
+			}
+
+			midResult.Sort();
+
+			if (range[0] > range[1])
+			{
+				int temp = range[0];
+				range[0] = range[1];
+				range[1] = temp;
+			}
+
+			foreach (int number in midResult)
+			{
+				if (number > range[0] && number < range[1])
+				{
+					result.Add(number);
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", result));
 		}
 	}
 }
