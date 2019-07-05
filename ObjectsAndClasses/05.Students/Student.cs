28ee33762a51fa9150e64c791713608045c67c4d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-05 11:46:59 +0300

    Add students

---
+++ ObjectsAndClasses/05.Students/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace _05.Students
+{
+	class Student
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public string City { get; set; }
+	}
+}
