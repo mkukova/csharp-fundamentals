2b80843aa3f1a0a7846df0a5557cb2e3b57c0aca
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-05 13:06:43 +0300

    Add store-boxes

---
+++ ObjectsAndClasses/07.StoreBoxes/Box.cs
@@ -0,0 +1,16 @@
+﻿namespace _07.StoreBoxes
+{
+	class Box
+	{
+		public Box()
+		{
+			Item = new Item();
+		}
+		public string SerialNumber { get; set; }
+		public string ItemName { get; set; }
+		public int Quantity { get; set; }
+		public double PriceForBox { get; set; }
+
+		public Item Item { get; set; }
+	}
+}
