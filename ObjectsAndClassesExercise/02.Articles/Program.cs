8e87dbe08a8709dcc2728de3a84324265d9f05dc
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-08 13:03:11 +0300

    Add articles

--- ObjectsAndClassesExercise/02.Articles/Program.cs
+++ ObjectsAndClassesExercise/02.Articles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace _02.Articles
 {
@@ -6,7 +8,33 @@ namespace _02.Articles
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine().Split(",",StringSplitOptions.RemoveEmptyEntries);
+			Article article = new Article(input[0], input[1], input[2]);
+
+			int numberCommands = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < numberCommands; i++)
+			{
+				string[] command = Console.ReadLine().Split(":");
+
+				switch (command[0])
+				{
+					case "Edit":
+						article.Edit(command[1]);
+						break;
+					case "ChangeAuthor":
+						article.ChangeAuthor(command[1]);
+						break;
+					case "Rename":
+						article.Rename(command[1]);
+						break;
+					default:
+						Console.WriteLine("Incorrect command!");
+						break;
+				}
+			}
+
+			article.AtricleToString();
 		}
 	}
 }
