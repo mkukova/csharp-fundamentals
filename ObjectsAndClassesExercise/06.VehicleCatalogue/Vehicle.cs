d756ed3d173ac25625336136f11eb2768d845ff9
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-09 15:03:34 +0300

    Add vehicle-catalogue

---
+++ ObjectsAndClassesExercise/06.VehicleCatalogue/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.VehicleCatalogue
+{
+	class Vehicle
+	{
+		public string Type { get; set; }
+		public string Model { get; set; }
+		public string Color { get; set; }
+		public double HorsePower { get; set; }
+
+		public Vehicle(string type, string model, string color, double horsePower)
+		{
+			this.Type = type;
+			this.Model = model;
+			this.Color = color;
+			this.HorsePower = horsePower;
+		}
+
+		public override string ToString()
+		{
+			string vehiclestr = $"Type: {(this.Type == "car" ? "Car" : "Truck")}{Environment.NewLine}" +
+
+								$"Model: {this.Model}{Environment.NewLine}" +
+
+								$"Color: {this.Color}{Environment.NewLine}" +
+
+								$"Horsepower: {this.HorsePower}";
+			return vehiclestr;
+		}
+	}
+}
