3d8b801980524403bb959b3f2ac867a459103ff4
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-09 12:03:16 +0300

    Add students-classes-more-exercise

--- ObjectsAndClassesExercise/04.Students/Program.cs
+++ ObjectsAndClassesExercise/04.Students/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04.Students
 {
@@ -6,7 +8,25 @@ namespace _04.Students
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int countOfStudents = int.Parse(Console.ReadLine());
+			List<Student> students = new List<Student>();
+ 
+			for (int i = 0; i < countOfStudents; i++)
+			{
+				string[] input = Console.ReadLine().Split(" ");
+				double currentGrade = double.Parse(input[2]);
+				Student currentStudent = new Student(input[0], input[1], currentGrade);
+				students.Add(currentStudent);
+			}
+
+			students = students.OrderBy(x => x.Grade).ToList();
+
+			students.Reverse();
+
+			for (int i = 0; i < countOfStudents; i++)
+			{
+				students[i].PrintInformation();
+			}
 		}
 	}
 }
