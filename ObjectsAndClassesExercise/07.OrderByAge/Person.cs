3c705254019e3bc5ef89cbe79b6e6c2b877c4232
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-09 15:16:38 +0300

    Add order-by-age

---
+++ ObjectsAndClassesExercise/07.OrderByAge/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.OrderByAge
+{
+	class Person
+	{
+		public string Name { get; set; }
+		public string ID { get; set; }
+		public int Age { get; set; }
+		public Person(string name, string id, int age)
+		{
+			this.Name = name;
+			this.ID = id;
+			this.Age = age;
+		}
+	}
+}
--- ObjectsAndClassesExercise/07.OrderByAge/Program.cs
+++ ObjectsAndClassesExercise/07.OrderByAge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.OrderByAge
 {
@@ -6,7 +8,32 @@ namespace _07.OrderByAge
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<Person> people = new List<Person>();
+
+			while (true)
+			{
+				string[] input = Console.ReadLine()
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+				if (input[0] == "End")
+				{
+					break;
+				}
+
+				string name = input[0];
+				string ID = input[1];
+				int age = int.Parse(input[2]);
+
+				Person currentPerson = new Person(name, ID, age);
+				people.Add(currentPerson);
+			}
+
+			people = people.OrderBy(x => x.Age).ToList();
+
+			foreach (var person in people)
+			{
+				Console.WriteLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+			}
 		}
 	}
 }
