8a9b7b60fd08bf4a9dbbc91f09f0478399159442
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-09 16:36:06 +0300

    Add speed-racing

---
+++ ObjectsAndClassesMoreExercise/03.SpeedRacing/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.SpeedRacing
+{
+	class Car
+	{
+		public string Model { get; set; }
+		public double FuelAmount { get; set; }
+		public double FuelConsumption { get; set; }
+		public double TraveledDistance { get; set; }
+		public void CanBeDriven(double km)
+		{
+			double neededFuel = this.FuelConsumption * km;
+
+			if (neededFuel <= this.FuelAmount)
+			{
+				this.FuelAmount -= neededFuel;
+				this.TraveledDistance += km;
+				
+			}
+			else
+			{
+				Console.WriteLine("Insufficient fuel for the drive");
+			}
+		}
+
+	}
+}
