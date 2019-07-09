c2c1382a8532ddb9c58dc7c07b2e35ed968a5e74
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-09 11:42:47 +0300

    Add articles-2.0

---
+++ ObjectsAndClassesExercise/03.Articles2.0/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Articles2._0
+{
+	class Article
+	{
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public string Author { get; set; }
+
+		public Article(string title, string content, string author)
+		{
+			this.Title = title;
+			this.Content = content;
+			this.Author = author;
+		}
+		public void AtricleToString()
+		{
+			Console.WriteLine($"{this.Title} -{this.Content}:{this.Author}");
+		}
+	}
+}
+
