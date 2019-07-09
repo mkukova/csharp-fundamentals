1837db6c47f9bea6892fc49bb584c6e354a16765
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-09 13:47:36 +0300

    Add teamwork-projects

---
+++ ObjectsAndClassesExercise/05.TeamworkProjects/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.TeamworkProjects
+{
+	class Team
+	{
+		public string CreatorName { get; set; }
+		public string TeamName { get; set; }
+		public List<string> Members { get; set; }
+
+		public Team(string creator, string name)
+		{
+			this.CreatorName = creator;
+			this.TeamName = name;
+		}
+	}
+}
