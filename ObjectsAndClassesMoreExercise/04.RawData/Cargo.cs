712b86626907fab3955f97a7258f9aa683070e40
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-10 13:29:38 +0300

    Add raw-data

---
+++ ObjectsAndClassesMoreExercise/04.RawData/Cargo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.RawData
+{
+	class Cargo
+	{
+		public int Weight { get; set; }
+		public string Type { get; set; }
+	}
+}
--- ObjectsAndClassesMoreExercise/04.RawData/Program.cs
+++ ObjectsAndClassesMoreExercise/04.RawData/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04.RawData
 {
@@ -6,7 +8,43 @@ namespace _04.RawData
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int numberOfCars = int.Parse(Console.ReadLine());
+			List<Car> cars = new List<Car>();
+
+			for (int i = 0; i < numberOfCars; i++)
+			{
+				string[] input = Console.ReadLine().Split(" ");
+				string model = input[0];
+				int engineSpeed = int.Parse(input[1]);
+				int enginePower = int.Parse(input[2]);
+				int cargoWeight = int.Parse(input[3]);
+				string cargoType = input[4];
+				Car currentCar = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType);
+				cars.Add(currentCar);
+			}
+
+			string command = Console.ReadLine();
+
+			if (command == "fragile")
+			{
+				List<Car> fragileCars = cars
+					.Where(x => x.Cargo.Weight < 1000 && x.Cargo.Type == "fragile")
+					.ToList();
+				foreach (var car in fragileCars)
+				{
+					Console.WriteLine(car.Model);
+				}
+			}
+			else if (command == "flamable")
+			{
+				List<Car> flamableCars = cars
+					.Where(x => x.Engine.Power > 250 && x.Cargo.Type == "flamable")
+					.ToList();
+				foreach (var car in flamableCars)
+				{
+					Console.WriteLine(car.Model);
+				}
+			}
 		}
 	}
 }
