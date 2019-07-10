ff07ab12cc954ff1f60f2c7114e7b03ffcfe2e52
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-10 14:40:10 +0300

    Add shopping-spree

--- ObjectsAndClassesMoreExercise/05.ShoppingSpree/Program.cs
+++ ObjectsAndClassesMoreExercise/05.ShoppingSpree/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.ShoppingSpree
 {
@@ -6,7 +8,48 @@ namespace _05.ShoppingSpree
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] allPeople = Console.ReadLine()
+				.Split(";", StringSplitOptions.RemoveEmptyEntries);
+			string[] allProducts = Console.ReadLine()
+				.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+			List<Person> people = new List<Person>();
+
+			for (int i = 0; i < allPeople.Length; i++)
+			{
+				string[] currentPerson = allPeople[i]
+					.Split("=", StringSplitOptions.RemoveEmptyEntries);
+				Person person = new Person(currentPerson[0], double.Parse(currentPerson[1]));
+				people.Add(person);
+			}
+
+			List<Product> products = new List<Product>();
+
+			for (int i = 0; i < allProducts.Length; i++)
+			{
+				string[] currentProduct = allProducts[i]
+					.Split("=", StringSplitOptions.RemoveEmptyEntries);
+				Product product = new Product(currentProduct[0], double.Parse(currentProduct[1]));
+				products.Add(product);
+			}
+
+			string command = Console.ReadLine();
+
+			while (command != "END")
+			{
+				string[] input = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+				string personName = input[0];
+				string productName = input[1];
+				Person buyer = people.FirstOrDefault(x => x.PersonName == personName);
+				Product product = products.FirstOrDefault(y => y.ProductName == productName);
+				buyer.BuyProduct(product);
+				command = Console.ReadLine();
+			}
+
+			foreach (var person in people)
+			{
+				Console.WriteLine(person);
+			}
 		}
 	}
 }
