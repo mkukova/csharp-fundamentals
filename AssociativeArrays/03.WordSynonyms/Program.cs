289d977efe47d0f680b1584d8e8b00f9db8961a0
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-12 16:35:06 +0300

    Add word-synonyms

--- AssociativeArrays/03.WordSynonyms/Program.cs
+++ AssociativeArrays/03.WordSynonyms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _03.WordSynonyms
 {
@@ -6,7 +8,24 @@ namespace _03.WordSynonyms
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = int.Parse(Console.ReadLine());
+			var words = new Dictionary<string, List<string>>();
+			for (int i = 0; i < n; i++)
+			{
+				string word = Console.ReadLine();
+				string synonyms = Console.ReadLine();
+				if (words.ContainsKey(word) == false)
+				{
+					words.Add(word, new List<string>());
+				}
+
+				words[word].Add(synonyms);
+			}
+
+			foreach (var word in words)
+			{
+				Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+			}
 		}
 	}
 }
