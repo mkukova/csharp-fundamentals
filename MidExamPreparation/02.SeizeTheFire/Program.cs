41d525d8269b018abcd3665f3e3e41a1be335621
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-12 14:49:01 +0300

    Add seize-the-fire

--- MidExamPreparation/02.SeizeTheFire/Program.cs
+++ MidExamPreparation/02.SeizeTheFire/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.SeizeTheFire
 {
@@ -6,7 +8,57 @@ namespace _02.SeizeTheFire
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] fires = Console.ReadLine()
+				.Split("#");
+
+			int water = int.Parse(Console.ReadLine());
+			double efforts = 0;
+			List<int> fireCells = new List<int>();
+
+			for (int i = 0; i < fires.Length; i++)
+			{
+				string[] fireArgs = fires[i]
+					.Split(" = ");
+				string type = fireArgs[0];
+				int fireValue = int.Parse(fireArgs[1]);
+				bool isCellValid = IsCellValid(type, fireValue);
+
+				if (isCellValid && water - fireValue >= 0)
+				{
+					efforts += fireValue * 0.25;
+					water -= fireValue;
+					fireCells.Add(fireValue);
+				}
+			}
+
+			int totalFire = fireCells.Sum();
+
+			Console.WriteLine($"Cells:");
+			foreach (var cell in fireCells)
+			{
+				Console.WriteLine($" - {cell}");
+			}
+
+			Console.WriteLine($"Effort: {efforts:F2}");
+			Console.WriteLine($"Total Fire: {totalFire}");
+		}
+
+		static bool IsCellValid(string type, int fireValue)
+		{
+			if (type == "High" && (fireValue >= 81 && fireValue <= 125))
+			{
+				return true;
+			}
+			if (type == "Medium" && (fireValue >= 51 && fireValue <= 80))
+			{
+				return true;
+			}
+			if (type == "Low" && (fireValue >= 1 && fireValue <= 50))
+			{
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
