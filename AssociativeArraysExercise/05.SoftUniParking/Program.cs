52d3555a37d6bdb8e487d1eeb30590ac9e673632
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-15 15:18:04 +0300

    Add softuni-parking

--- AssociativeArraysExercise/05.SoftUniParking/Program.cs
+++ AssociativeArraysExercise/05.SoftUniParking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.SoftUniParking
 {
@@ -6,7 +8,44 @@ namespace _05.SoftUniParking
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var userPlateNumber = new Dictionary<string, string>();
+			int comandsCount = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < comandsCount; i++)
+			{
+				string[] commandArgs = Console.ReadLine().Split(" ");
+				string command = commandArgs[0];
+				string name = commandArgs[1];
+
+				if (command == "register")
+				{
+					string plateNumber = commandArgs[2];
+
+					if (!userPlateNumber.ContainsKey(name))
+					{
+						userPlateNumber[name] = plateNumber;
+						Console.WriteLine($"{name} registered {plateNumber} successfully");
+					}
+					else
+					{
+						Console.WriteLine($"ERROR: already registered with plate number {plateNumber}");
+					}
+				}
+				else
+				{
+					if (!userPlateNumber.ContainsKey(name))
+					{
+						Console.WriteLine($"ERROR: user {name} not found");
+					}
+					else
+					{
+						userPlateNumber.Remove(name);
+						Console.WriteLine($"{name} unregistered successfully");
+					}
+				}
+			}
+
+			Console.WriteLine(string.Join(Environment.NewLine, userPlateNumber.Select(x => $"{x.Key} => {x.Value}")));
 		}
 	}
 }
