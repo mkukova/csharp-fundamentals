832ce28b81d3d74945d9b0a124fa101b3ffc21c2
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-15 14:40:09 +0300

    Add legendary-farming

--- AssociativeArraysExercise/03.LegendaryFarming/Program.cs
+++ AssociativeArraysExercise/03.LegendaryFarming/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _03.LegendaryFarming
 {
@@ -6,7 +8,85 @@ namespace _03.LegendaryFarming
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var keyMaterials = new Dictionary<string, int>()
+			{
+				["shards"] = 0,
+				["fragments"] = 0,
+				["motes"] = 0
+			};
+
+			var junkMaterials = new Dictionary<string, int>();
+			string winner = string.Empty;
+			bool hasToBreak = false;
+
+			while (true)
+			{
+				string[] input = Console.ReadLine()
+				.ToLower()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.ToArray();
+
+				for (int i = 0; i < input.Length; i += 2)
+				{
+					string material = input[i + 1];
+					int quantity = int.Parse(input[i]);
+
+					if (keyMaterials.ContainsKey(material))
+					{
+						keyMaterials[material] += quantity;
+
+						if (keyMaterials[material] >= 250)
+						{
+							winner = material;
+							hasToBreak = true;
+							break;
+						}
+					}
+					else
+					{
+						if (!junkMaterials.ContainsKey(material))
+						{
+							junkMaterials[material] = 0;
+						}
+
+						junkMaterials[material] += quantity;
+					}
+				}
+
+				if (hasToBreak)
+				{
+					break;
+				}
+			}
+
+			switch (winner)
+			{
+				case "shards":
+					Console.WriteLine("Shadowmourne obtained!");
+					keyMaterials[winner] -= 250;
+					break;
+				case "fragments":
+					Console.WriteLine("Valanyr obtained!");
+					keyMaterials[winner] -= 250;
+					break;
+				case "motes":
+					Console.WriteLine("Dragonwrath obtained!");
+					keyMaterials[winner] -= 250;
+					break;
+			}
+
+			var firstPartOfResult = keyMaterials.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+			var secondPartOfResult = junkMaterials.OrderBy(x => x.Key);
+
+			foreach (var item in firstPartOfResult)
+			{
+				Console.WriteLine($"{item.Key}: {item.Value}");
+			}
+
+			foreach (var item in secondPartOfResult)
+			{
+				Console.WriteLine($"{item.Key}: {item.Value}");
+			}
 		}
 	}
 }
