20d2959d86881e6f1630111a778fd0350ed6caf1
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-15 14:57:46 +0300

    Add orders

--- AssociativeArraysExercise/04.Orders/Program.cs
+++ AssociativeArraysExercise/04.Orders/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04.Orders
 {
@@ -6,7 +8,34 @@ namespace _04.Orders
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var products = new Dictionary<string, double[]>();
+
+			while (true)
+			{
+				string[] input = Console.ReadLine().Split(" ");
+
+				if (input[0] == "buy")
+				{
+					break;
+				}
+
+				string productName = input[0];
+				double price = double.Parse(input[1]);
+				int quantity = int.Parse(input[2]);
+
+				if (!products.ContainsKey(productName))
+				{
+					products.Add(productName, new double[2]);
+				}
+
+				products[productName][0] = price;
+				products[productName][1] += quantity;
+			}
+
+			foreach (var item in products)
+			{
+				Console.WriteLine($"{item.Key} -> {(item.Value[0] * item.Value[1]):F2}");
+			}
 		}
 	}
 }
