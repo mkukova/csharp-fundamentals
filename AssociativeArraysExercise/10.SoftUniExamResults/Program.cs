4c2565e7e970292b0718c69b2bc2912e4e4de835
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-16 12:07:45 +0300

    Add softuni-exam-results

--- AssociativeArraysExercise/10.SoftUniExamResults/Program.cs
+++ AssociativeArraysExercise/10.SoftUniExamResults/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _10.SoftUniExamResults
 {
@@ -6,7 +8,77 @@ namespace _10.SoftUniExamResults
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var namesAndPoints = new Dictionary<string, double>();
+			var counterForLanguages = new Dictionary<string, int>();
+			double current = 0;
+
+			while (true)
+			{
+				string[] splitedCommand = Console.ReadLine().Split("-");
+				string name = splitedCommand[0];
+
+				if (splitedCommand[0] == "exam finished")
+				{
+					break;
+				}
+
+				string language = splitedCommand[1];
+
+				if (language == "banned")
+				{
+					namesAndPoints.Remove(name);
+					continue;
+				}
+
+				double points = double.Parse(splitedCommand[2]);
+
+				if (!namesAndPoints.ContainsKey(name))
+				{
+					namesAndPoints[name] = points;
+					current = points;
+				}
+				else
+				{
+					current = namesAndPoints[name];
+
+					if (current < points)
+					{
+						current = points;
+						namesAndPoints[name] = current;
+					}
+				}
+
+				if (!counterForLanguages.ContainsKey(language))
+				{
+					counterForLanguages[language] = 1;
+				}
+				else
+				{
+					counterForLanguages[language]++;
+				}
+			}
+
+			namesAndPoints = namesAndPoints
+				.OrderByDescending(x => x.Value)
+				.ThenBy(x => x.Key)
+				.ToDictionary(x => x.Key, x => x.Value);
+
+			Console.WriteLine($"Results:");
+			foreach (var item in namesAndPoints)
+			{
+				Console.WriteLine($"{item.Key} | {item.Value}");
+			}
+
+			counterForLanguages = counterForLanguages
+				.OrderByDescending(x => x.Value)
+				.ThenBy(x => x.Key)
+				.ToDictionary(x => x.Key, x => x.Value);
+
+			Console.WriteLine("Submissions:");
+			foreach (var item in counterForLanguages)
+			{
+				Console.WriteLine($"{item.Key} - {item.Value}");
+			}
 		}
 	}
 }
