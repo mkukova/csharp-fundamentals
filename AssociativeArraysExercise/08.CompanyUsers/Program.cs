8f3568454d2a4f2fffe48760beea38e7cb14a4ea
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-16 10:50:53 +0300

    Add company-users

--- AssociativeArraysExercise/08.CompanyUsers/Program.cs
+++ AssociativeArraysExercise/08.CompanyUsers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _08.CompanyUsers
 {
@@ -6,7 +8,42 @@ namespace _08.CompanyUsers
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var companiesList = new Dictionary<string, List<string>>();
+
+			while (true)
+			{
+				string[] input = Console.ReadLine().Split(" -> ");
+
+				if (input[0] == "End")
+				{
+					break;
+				}
+
+				string companyName = input[0];
+				string employeeID = input[1];
+
+				if (!companiesList.ContainsKey(companyName))
+				{
+					companiesList[companyName] = new List<string>();
+				}
+
+				if (!companiesList[companyName].Contains(employeeID))
+				{
+					companiesList[companyName].Add(employeeID);
+				}
+			}
+
+			var orderedCompanyList = companiesList.OrderBy(x => x.Key);
+
+			foreach (var company in orderedCompanyList)
+			{
+				Console.WriteLine($"{company.Key}");
+
+				foreach (var employy in company.Value)
+				{
+					Console.WriteLine($"-- {employy}");
+				}
+			}
 		}
 	}
 }
