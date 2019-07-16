ab167b8d52110a258c2f0d63bbdecde740202cae
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-16 10:39:40 +0300

    Add student-academy

--- AssociativeArraysExercise/07.StudentAcademy/Program.cs
+++ AssociativeArraysExercise/07.StudentAcademy/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _07.StudentAcademy
 {
@@ -6,7 +8,27 @@ namespace _07.StudentAcademy
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int studentsNumber = int.Parse(Console.ReadLine());
+			var studentsList = new Dictionary<string, List<double>>();
+
+			for (int i = 0; i < studentsNumber; i++)
+			{
+				string name = Console.ReadLine();
+				double grade = double.Parse(Console.ReadLine());
+
+				if (!studentsList.ContainsKey(name))
+				{
+					studentsList[name] = new List<double>();
+				}
+
+				studentsList[name].Add(grade);
+			}
+
+			var filteredStudents = studentsList.Where(x => x.Value.Average() >= 4.50);
+
+			filteredStudents = filteredStudents.OrderByDescending(x => x.Value.Average());
+
+			Console.WriteLine(string.Join(Environment.NewLine, filteredStudents.Select(x => $"{x.Key} -> {(x.Value.Average()):F2}")));
 		}
 	}
 }
