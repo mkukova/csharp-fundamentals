f6ab08d5fff177ad06d3f4345e8e5a4b254a8323
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-17 13:28:48 +0300

    Add moba-challenger

--- AssociativeArraysMoreExercise/03.MOBAChallenger/Program.cs
+++ AssociativeArraysMoreExercise/03.MOBAChallenger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _03.MOBAChallenger
 {
@@ -6,7 +8,97 @@ namespace _03.MOBAChallenger
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var playersList = new Dictionary<string, Dictionary<string, int>>();
+
+			while (true)
+			{
+				string input = Console.ReadLine();
+
+				if (input == "Season end")
+				{
+					break;
+				}
+
+				if (input.Contains("->"))
+				{
+					string[] splittedInput = input.Split(" -> ");
+					string player = splittedInput[0];
+					string position = splittedInput[1];
+					int skill = int.Parse(splittedInput[2]);
+
+					if (!playersList.ContainsKey(player))
+					{
+						playersList[player] = new Dictionary<string, int>();
+					}
+
+					if (!playersList[player].ContainsKey(position))
+					{
+						playersList[player].Add(position, 0);
+					}
+
+					if (playersList[player][position] <= skill)
+					{
+						playersList[player][position] = skill;
+					}
+				}
+				else
+				{
+					string[] splittedInput = input.Split(" ");
+					string player1 = splittedInput[0];
+					string player2 = splittedInput[2];
+
+					if (playersList.ContainsKey(player1) && playersList.ContainsKey(player2))
+					{
+						List<string> player1Positions = playersList[player1].Keys.ToList();
+						List<string> player2Positions = playersList[player2].Keys.ToList();
+						string position = "";
+						bool commonPosition = false;
+
+						for (int i = 0; i < player1Positions.Count; i++)
+						{
+							if (player2Positions.Contains(player1Positions[i]))
+							{
+								position = player1Positions[i];
+								commonPosition = true;
+								break;
+							}
+						}
+
+						if (commonPosition)
+						{
+							int totalSkills1 = playersList[player1].Values.Sum();
+							int totalSkills2 = playersList[player2].Values.Sum();
+
+							if (totalSkills1 > totalSkills2)
+							{
+								playersList.Remove(player2);
+							}
+							else if (totalSkills1 < totalSkills2)
+							{
+								playersList.Remove(player1);
+							}
+						}
+					}
+					else
+					{
+						continue;
+					}
+				}
+			}
+
+			foreach (var item in playersList
+				.OrderByDescending(x => x.Value.Values.Sum())
+				.ThenBy(y => y.Key))
+			{
+				Console.WriteLine($"{item.Key}: {item.Value.Values.Sum()} skill");
+
+				foreach (var player in item.Value
+					.OrderByDescending(x => x.Value)
+					.ThenBy(y => y.Key))
+				{
+					Console.WriteLine($"- {player.Key} <::> {player.Value}");
+				}
+			}
 		}
 	}
 }
