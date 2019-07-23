79fbaa327099f3bceab7564b9c589c112668b0b5
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-23 15:11:30 +0300

    Add character-multiplier

--- TextProcessingExercise/02.CharacterMultiplier/Program.cs
+++ TextProcessingExercise/02.CharacterMultiplier/Program.cs
@@ -6,7 +6,33 @@ namespace _02.CharacterMultiplier
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine().Split(" ");
+			string firstString = input[0];
+			string secondString = input[1];
+			int sum = 0;
+
+			for (int i = 0; i < Math.Min(firstString.Length, secondString.Length); i++)
+			{
+				sum += (int)firstString[i] * (int)secondString[i];
+			}
+
+			if (firstString.Length > secondString.Length)
+			{
+				for (int i = secondString.Length; i < firstString.Length; i++)
+				{
+					sum += (int)firstString[i];
+				}
+			}
+			else if (firstString.Length < secondString.Length)
+			{
+
+				for (int i = firstString.Length; i < secondString.Length; i++)
+				{
+					sum += (int)secondString[i];
+				}
+			}
+
+			Console.WriteLine(sum);
 		}
 	}
 }
