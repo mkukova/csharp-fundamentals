7f2a2b425d82f07f0e88d8a17ca13999a95a790c
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-24 12:47:17 +0300

    Save replace-repeating-chars

--- TextProcessingExercise/06.ReplaceRepeatingChars/Program.cs
+++ TextProcessingExercise/06.ReplaceRepeatingChars/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
 
 namespace _06.ReplaceRepeatingChars
 {
@@ -6,7 +8,45 @@ namespace _06.ReplaceRepeatingChars
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string input = Console.ReadLine().ToString();
+			var result = new StringBuilder();
+
+			if (input == string.Empty)
+			{
+				Console.WriteLine(" ");
+				return;
+			}
+
+			int inputLenght = input.Length-1;
+
+			if (inputLenght==0)
+			{
+				Console.WriteLine(input);
+				return;
+			}
+
+			for (int i = 0; i < inputLenght - 1; i++)
+			{
+				char symbol = input[i];
+
+				if (symbol != input[i + 1])
+				{
+					result.Append(symbol);
+
+					if ((i + 1) == (inputLenght - 1))
+					{
+						result.Append(input[i + 1]);
+					}
+				}
+			}
+
+			if (input[inputLenght] != input[inputLenght - 1])
+			{
+				result.Append(input[inputLenght]);
+			}
+
+			Console.WriteLine(result.ToString());
 		}
 	}
 }
+
