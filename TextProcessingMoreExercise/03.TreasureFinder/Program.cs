87534efa2ae29ac4ae69114a2dcd8a1873df804c
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-24 14:55:33 +0300

    Add treasure-finder

--- TextProcessingMoreExercise/03.TreasureFinder/Program.cs
+++ TextProcessingMoreExercise/03.TreasureFinder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.TreasureFinder
 {
@@ -6,7 +7,47 @@ namespace _03.TreasureFinder
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] keyNumbers = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			string input = Console.ReadLine();
+
+			while (input != "find")
+			{
+				string result = string.Empty;
+				int inputLenght = input.Length;
+				int counter = 0;
+
+				while (counter < inputLenght)
+				{
+					for (int i = 0; i < keyNumbers.Length; i++)
+					{
+						int currentChar = (int)input[counter];
+						int newChar = currentChar - keyNumbers[i];
+						result += (char)newChar;
+						counter++;
+						if (counter == inputLenght)
+						{
+							break;
+						}
+					}
+				}
+
+				int indexStartType = result.IndexOf('&');
+				string searchType = result.Substring(indexStartType + 1);
+				int indexEndType = searchType.IndexOf('&');
+				string type = result.Substring(indexStartType + 1, indexEndType);
+
+				int indexStartCoordinates = result.IndexOf('<');
+				int indexEndCoordinates = result.IndexOf('>');
+				int coordinatesLenght = indexEndCoordinates - indexStartCoordinates - 1;
+				string coordinates = result.Substring(indexStartCoordinates + 1, coordinatesLenght);
+
+				Console.WriteLine($"Found {type} at {coordinates}");
+				input = Console.ReadLine();
+			}
 		}
 	}
 }
