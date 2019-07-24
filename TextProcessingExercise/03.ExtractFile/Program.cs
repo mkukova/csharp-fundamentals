2b7454b20c6b0a38f9a5bd7ce4caf024c10e5370
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-24 10:59:11 +0300

    Add extract-file

--- TextProcessingExercise/03.ExtractFile/Program.cs
+++ TextProcessingExercise/03.ExtractFile/Program.cs
@@ -6,7 +6,13 @@ namespace _03.ExtractFile
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine().Split("\\");
+			string file = input[input.Length - 1];
+			string fileName = file.Substring(0, file.LastIndexOf('.'));
+			string fileExtension = file.Substring(file.LastIndexOf('.') + 1);
+
+			Console.WriteLine($"File name: {fileName}");
+			Console.WriteLine($"File extension: {fileExtension}");
 		}
 	}
 }
