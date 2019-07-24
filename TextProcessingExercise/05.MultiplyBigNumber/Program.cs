81ca991988d710b7aab719a4ab7adfcfc4d3b9fb
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-24 11:28:21 +0300

    Add multiply-big-number

--- TextProcessingExercise/05.MultiplyBigNumber/Program.cs
+++ TextProcessingExercise/05.MultiplyBigNumber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
 
 namespace _05.MultiplyBigNumber
 {
@@ -6,7 +8,37 @@ namespace _05.MultiplyBigNumber
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string numberAsString = Console.ReadLine();
+			int multiplier = int.Parse(Console.ReadLine());
+			var builder = new StringBuilder();
+			int onMind = 0;
+
+			for (int i = numberAsString.Length - 1; i >= 0; i--)
+			{
+				int lastDigit = int.Parse(numberAsString[i].ToString());
+				int result = lastDigit * multiplier + onMind;
+				builder.Append(result % 10);
+				onMind = result / 10;
+			}
+
+			if (onMind != 0)
+			{
+				builder.Append(onMind);
+			}
+
+			string resultNumber = string.Join("", builder
+				.ToString()
+				.Reverse())
+				.TrimStart('0');
+
+			if (resultNumber == string.Empty)
+			{
+				Console.WriteLine(0);
+			}
+			else
+			{
+				Console.WriteLine(resultNumber);
+			}
 		}
 	}
 }
