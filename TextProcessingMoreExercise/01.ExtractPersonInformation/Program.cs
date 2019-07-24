52195777a73acc8ecfa4289b4481b205dbfa6ae5
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-24 14:06:42 +0300

    Add extract-person-information

--- TextProcessingMoreExercise/01.ExtractPersonInformation/Program.cs
+++ TextProcessingMoreExercise/01.ExtractPersonInformation/Program.cs
@@ -6,7 +6,21 @@ namespace _01.ExtractPersonInformation
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int linesOfInformation = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < linesOfInformation; i++)
+			{
+				string input = Console.ReadLine();
+				int startIndexName = input.IndexOf('@');
+				int endIndexName = input.IndexOf('|');
+				int startIndexAge = input.IndexOf('#');
+				int endIndexAge = input.IndexOf('*');
+
+				string name = input.Substring(startIndexName + 1, endIndexName - startIndexName - 1);
+				string age = input.Substring(startIndexAge + 1, endIndexAge - startIndexAge - 1);
+
+				Console.WriteLine($"{name} is {age} years old.");
+			}
 		}
 	}
 }
