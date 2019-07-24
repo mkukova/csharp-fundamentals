a7c37c95a028c2569821c2571d01174c0b33d21e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-24 15:18:25 +0300

    Add morse-code-translator

--- TextProcessingMoreExercise/04.MorseCodeTranslator/Program.cs
+++ TextProcessingMoreExercise/04.MorseCodeTranslator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04.MorseCodeTranslator
 {
@@ -6,7 +7,103 @@ namespace _04.MorseCodeTranslator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine().Split(" ");
+			string result = string.Empty;
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				string currentCode = input[i];
+				char currentLetter = ' ';
+
+				switch (currentCode)
+				{
+					case ".-":
+						currentLetter = 'A';
+						break;
+					case "-...":
+						currentLetter = 'B';
+						break;
+					case "-.-.":
+						currentLetter = 'C';
+						break;
+					case "-..":
+						currentLetter = 'D';
+						break;
+					case ".":
+						currentLetter = 'E';
+						break;
+					case "..-.":
+						currentLetter = 'F';
+						break;
+					case "--.":
+						currentLetter = 'G';
+						break;
+					case "....":
+						currentLetter = 'H';
+						break;
+					case "..":
+						currentLetter = 'I';
+						break;
+					case ".---":
+						currentLetter = 'J';
+						break;
+					case "-.-":
+						currentLetter = 'K';
+						break;
+					case ".-..":
+						currentLetter = 'L';
+						break;
+					case "--":
+						currentLetter = 'M';
+						break;
+					case "-.":
+						currentLetter = 'N';
+						break;
+					case "---":
+						currentLetter = 'O';
+						break;
+					case ".--.":
+						currentLetter = 'P';
+						break;
+					case "--.-":
+						currentLetter = 'Q';
+						break;
+					case ".-.":
+						currentLetter = 'R';
+						break;
+					case "...":
+						currentLetter = 'S';
+						break;
+					case "-":
+						currentLetter = 'T';
+						break;
+					case "..-":
+						currentLetter = 'U';
+						break;
+					case "...-":
+						currentLetter = 'V';
+						break;
+					case ".--":
+						currentLetter = 'W';
+						break;
+					case "-..-":
+						currentLetter = 'X';
+						break;
+					case "-.--":
+						currentLetter = 'Y';
+						break;
+					case "--..":
+						currentLetter = 'Z';
+						break;
+					case "|":
+						currentLetter = ' ';
+						break;
+				}
+
+				result += currentLetter;
+			}
+
+			Console.WriteLine(result);
 		}
 	}
 }
