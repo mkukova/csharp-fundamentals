71e351c1602b840d057d8278eaba8bb68cedf0fe
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-24 14:14:44 +0300

    Add ascii-sumator

--- TextProcessingMoreExercise/02.AsciiSumator/Program.cs
+++ TextProcessingMoreExercise/02.AsciiSumator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.AsciiSumator
 {
@@ -6,7 +7,37 @@ namespace _02.AsciiSumator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			char firstCharacter = char.Parse(Console.ReadLine());
+			char secondCharacter = char.Parse(Console.ReadLine());
+			string text = Console.ReadLine();
+			int sum = 0;
+			int firstCharAsciiCode = (int)firstCharacter;
+			int secondCharAsciiCode = (int)secondCharacter;
+			int biggerNumber = 0;
+			int smallerNumber = 0;
+
+			if (firstCharAsciiCode > secondCharAsciiCode)
+			{
+				biggerNumber = firstCharAsciiCode;
+				smallerNumber = secondCharAsciiCode;
+			}
+			else
+			{
+				biggerNumber = secondCharAsciiCode;
+				smallerNumber = firstCharAsciiCode;
+			}
+
+			for (int i = 0; i < text.Length; i++)
+			{
+				int currentAsciiCode = (int)text[i];
+
+				if (currentAsciiCode > smallerNumber && currentAsciiCode < biggerNumber)
+				{
+					sum += currentAsciiCode;
+				}
+			}
+
+			Console.WriteLine(sum);
 		}
 	}
 }
