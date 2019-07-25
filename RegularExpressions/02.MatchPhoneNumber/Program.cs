1288797915cb176569a84a890bc600788774404a
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-25 13:47:08 +0300

    Add match-phone-number

--- RegularExpressions/02.MatchPhoneNumber/Program.cs
+++ RegularExpressions/02.MatchPhoneNumber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace _02.MatchPhoneNumber
 {
@@ -6,7 +8,14 @@ namespace _02.MatchPhoneNumber
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string input = Console.ReadLine();
+			string pattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+			var phoneMatches = Regex.Matches(input, pattern);
+			var matchedPhones = phoneMatches
+				.Cast<Match>()
+				.Select(x => x.Value.Trim())
+				.ToArray();
+			Console.WriteLine(string.Join(", ", matchedPhones));
 		}
 	}
 }
