298ab8bdc900e760ccfdd997adcaa104d67edd45
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-25 13:29:01 +0300

    Change replace-repeating-chars

---
+++ RegularExpressions/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string phoneNumbers = Console.ReadLine();
+			string pattern = @"\+\d{3}( |-)[0-9]\1\d{3}\1\d{4}\b";
+			MatchCollection phoneMatches = Regex.Matches(phoneNumbers, pattern);
+			var matchedPhones = phoneMatches
+				.Cast<Match>()
+				.Select(x => x.Value.Trim())
+				.ToArray();
+
+			Console.WriteLine(string.Join(", ", matchedPhones));
+		}
+	}
+}
