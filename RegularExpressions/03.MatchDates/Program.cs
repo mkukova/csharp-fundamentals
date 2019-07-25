95c1e96ce4eee28f56ca9c77bed1a441a1857515
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-25 14:03:29 +0300

    Add match-dates

--- RegularExpressions/03.MatchDates/Program.cs
+++ RegularExpressions/03.MatchDates/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace _03.MatchDates
 {
@@ -6,7 +8,17 @@ namespace _03.MatchDates
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string pattern = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+			string dates = Console.ReadLine();
+			var matchedDates = Regex.Matches(dates, pattern);
+
+			foreach (Match date in matchedDates)
+			{
+				var day = date.Groups["day"].Value;
+				var month = date.Groups["month"].Value;
+				var year = date.Groups["year"].Value;
+				Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+			}
 		}
 	}
 }
