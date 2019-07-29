ec5547d02d08e1b5e74247332cada15d1a76ec6d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-29 14:18:15 +0300

    Add star-enigma

--- RegularExpressionsExercise/04.StarEnigma/Program.cs
+++ RegularExpressionsExercise/04.StarEnigma/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04.StarEnigma
 {
@@ -6,7 +9,54 @@ namespace _04.StarEnigma
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int countMessages = int.Parse(Console.ReadLine());
+			string firstPattern = @"[starSTAR]";
+			List<string> attackedPlanets = new List<string>();
+			List<string> destroyedPlanets = new List<string>();
+
+			for (int i = 0; i < countMessages; i++)
+			{
+				string message = Console.ReadLine();
+				MatchCollection lettersMatched = Regex.Matches(message, firstPattern);
+				int countOfLetters = lettersMatched.Count();
+				string newMessage = string.Empty;
+
+				foreach (var letter in message)
+				{
+					newMessage += (char)(letter - countOfLetters);
+				}
+
+				string plannetPattern = @"@(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>[0-9]+)[^@\-!:>]*!(?<type>A|D)![^@\-!:>]*->(?<soldierCount>[0-9]+)";
+
+				Match planetArgs = Regex.Match(newMessage, plannetPattern);
+
+				if (planetArgs.Success)
+				{
+					string planetName = planetArgs.Groups["name"].Value;
+					string type = planetArgs.Groups["type"].Value;
+
+					if (type == "A")
+					{
+						attackedPlanets.Add(planetName);
+					}
+					else if (type == "D")
+					{
+						destroyedPlanets.Add(planetName);
+					}
+				}
+			}
+
+			Console.WriteLine($"Attacked planets: {attackedPlanets.Count()}");
+			foreach (var planet in attackedPlanets.OrderBy(x => x))
+			{
+				Console.WriteLine($"-> {planet}");
+			}
+
+			Console.WriteLine($"Destroyed planets: {destroyedPlanets.Count()}");
+			foreach (var planet in destroyedPlanets.OrderBy(x => x))
+			{
+				Console.WriteLine($"-> {planet}");
+			}
 		}
 	}
 }
