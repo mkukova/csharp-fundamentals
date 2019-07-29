2ac2d41156a167debe61d5e2e4830dbdd656d190
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-29 13:05:33 +0300

    Add softuni-bar-income

--- RegularExpressionsExercise/03.SoftUniBarIncome/Program.cs
+++ RegularExpressionsExercise/03.SoftUniBarIncome/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _03.SoftUniBarIncome
 {
@@ -6,7 +9,32 @@ namespace _03.SoftUniBarIncome
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+			double totalIncome = 0;
+
+			while (true)
+			{
+				string input = Console.ReadLine();
+
+				if (input == "end of shift")
+				{
+					break;
+				}
+
+				if (Regex.IsMatch(input, pattern))
+				{
+					Match match = Regex.Match(input, pattern);
+					string customer = match.Groups["customer"].Value;
+					string product = match.Groups["product"].Value;
+					int count = int.Parse(match.Groups["count"].Value);
+					double price = double.Parse(match.Groups["price"].Value);
+					double money = price * count;
+					Console.WriteLine($"{customer}: {product} - {money:F2}");
+					totalIncome += money;
+				}
+			}
+
+			Console.WriteLine($"Total income: {totalIncome:F2}");
 		}
 	}
 }
