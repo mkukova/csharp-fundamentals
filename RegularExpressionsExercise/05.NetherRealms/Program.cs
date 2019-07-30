ff17aad140f508e8c668e941926b2790544b56f3
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-30 10:53:13 +0300

    Add nether-realms

--- RegularExpressionsExercise/05.NetherRealms/Program.cs
+++ RegularExpressionsExercise/05.NetherRealms/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace _05.NetherRealms
 {
@@ -6,7 +9,61 @@ namespace _05.NetherRealms
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Regex lettersRegex = new Regex(@"([^\d\+\-\*/\.])");
+			Regex numbersRegex = new Regex(@"((\+|\-)?(\d+(\.\d+)?))");
+			Regex multAndDivRegex = new Regex(@"(/|\*)");
+
+			List<string> demons = Console.ReadLine()
+								  .Split(new char[] { ',', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+								  .ToList();
+
+			foreach (var demon in demons.OrderBy(n => n))
+			{
+				string demonLetters = "";
+
+				foreach (var letter in lettersRegex.Matches(demon))
+				{
+					demonLetters += letter.ToString();
+				}
+
+				List<Match> numbers = numbersRegex.Matches(demon).ToList();
+
+				decimal damage = 0;
+
+				if (numbers.Count > 0)
+				{
+					foreach (var num in numbers)
+					{
+						damage += decimal.Parse(num.ToString());
+					}
+
+					MatchCollection multiAndDiv = multAndDivRegex.Matches(demon);
+
+					if (multiAndDiv.Count() > 0)
+					{
+						foreach (var symbol in multiAndDiv)
+						{
+							if (symbol.ToString() == "*")
+							{
+								damage *= 2;
+							}
+							else if (symbol.ToString() == "/")
+							{
+								damage /= 2;
+							}
+						}
+					}
+				}
+
+				int health = 0;
+
+				foreach (var symbol in demonLetters)
+				{
+					health += symbol;
+				}
+
+				Console.WriteLine($"{demon} - {health} health, {damage:F2} damage");
+			}
 		}
 	}
 }
