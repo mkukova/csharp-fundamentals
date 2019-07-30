0d660d654d2408042962c835387f321bed04ffc2
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-30 13:26:23 +0300

    Add post-office

--- RegularExpressionsMoreExercise/3.PostOffice/Program.cs
+++ RegularExpressionsMoreExercise/3.PostOffice/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Generic;
 
 namespace _3.PostOffice
 {
@@ -6,7 +10,31 @@ namespace _3.PostOffice
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine().Split("|");
+
+			string firstPart = input[0];
+			string secondPart = input[1];
+			string thirdPart = input[2];
+
+			string firstPattern = @"([#$%*&])(?<capitals>[A-Z]+)(\1)";
+			Match firstMatch = Regex.Match(firstPart, firstPattern);
+			string capitals = firstMatch.Groups["capitals"].Value;
+
+			for (int index = 0; index < capitals.Length; index++)
+			{
+				char startLetter = capitals[index];
+				int ASCIIcode = startLetter;
+
+				string secondPattern = $@"{ASCIIcode}:(?<length>[0-9][0-9])";
+				Match secondMatch = Regex.Match(secondPart, secondPattern);
+				int length = int.Parse(secondMatch.Groups["length"].Value);
+
+				string thirdPattern = $@"(?<=\s|^){startLetter}[^\s]{{{length}}}(?=\s|$)";
+				Match thirdMatch = Regex.Match(thirdPart, thirdPattern);
+				string word = thirdMatch.ToString();
+
+				Console.WriteLine(word);
+			}
 		}
 	}
 }
