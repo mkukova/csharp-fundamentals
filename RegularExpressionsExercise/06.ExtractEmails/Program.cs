0c2ac9db848b3a453ef8d49b1aebce0e09fef055
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-30 11:18:55 +0300

    Add extract-emails

--- RegularExpressionsExercise/06.ExtractEmails/Program.cs
+++ RegularExpressionsExercise/06.ExtractEmails/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace _06.ExtractEmails
 {
@@ -6,7 +10,9 @@ namespace _06.ExtractEmails
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Regex emailRegex = new Regex(@"(\s[a-z]+[\w.-]+\w)@([a-z]+[-a-z]*?([.][a-z]+)+)\b");
+			string input = Console.ReadLine();
+			Console.WriteLine(string.Join(Environment.NewLine, emailRegex.Matches(input)));
 		}
 	}
 }
