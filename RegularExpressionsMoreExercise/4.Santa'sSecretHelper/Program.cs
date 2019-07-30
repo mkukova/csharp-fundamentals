84bd84689ef7b39b9bef596771a5f5b4fd4d2adc
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-30 13:45:30 +0300

    Add santa's-secret-helper

--- RegularExpressionsMoreExercise/4.Santa'sSecretHelper/Program.cs
+++ RegularExpressionsMoreExercise/4.Santa'sSecretHelper/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _4.Santa_sSecretHelper
 {
@@ -6,7 +10,37 @@ namespace _4.Santa_sSecretHelper
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int key = int.Parse(Console.ReadLine());
+			string pattern = @"@(?<name>[A-Za-z]+)[^@\-!:>]+?!(?<behaviour>[N|G])!";
+			string input = Console.ReadLine();
+			List<string> goodChildren = new List<string>();
+
+			while (input != "end")
+			{
+				var newMessage = new StringBuilder();
+
+				foreach (var letter in input)
+				{
+					newMessage.Append((char)(letter - key));
+				}
+
+				string newMessageAsStr = newMessage.ToString();
+
+				Match match = Regex.Match(newMessageAsStr, pattern);
+
+				if (match.Success)
+				{
+					if (match.Groups["behaviour"].Value == "G")
+					{
+						string name = match.Groups["name"].Value;
+						goodChildren.Add(name);
+					}
+				}
+
+				input = Console.ReadLine();
+			}
+
+			Console.WriteLine(string.Join(Environment.NewLine, goodChildren));
 		}
 	}
 }
