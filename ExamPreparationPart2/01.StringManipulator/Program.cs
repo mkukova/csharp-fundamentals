dbe59fb94a13f398976477d998ca79a97832777b
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-06 16:00:37 +0300

    Add string-manipulator

--- ExamPreparationPart2/01.StringManipulator/Program.cs
+++ ExamPreparationPart2/01.StringManipulator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _01.StringManipulator
 {
@@ -6,7 +9,61 @@ namespace _01.StringManipulator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string result = string.Empty;
+			string input = Console.ReadLine();
+
+			while (input != "End")
+			{
+				string[] splittedInput = input.Split(" ");
+				string command = splittedInput[0];
+
+				if (command == "Add")
+				{
+					string newStrToAdd = splittedInput[1];
+					result += newStrToAdd;
+				}
+				else if (command == "Upgrade")
+				{
+					char oldChar = char.Parse(splittedInput[1]);
+					result = result.Replace(oldChar, (char)(oldChar + 1));
+				}
+				else if (command == "Print")
+				{
+					Console.WriteLine(result);
+				}
+				else if (command == "Index")
+				{
+					char symbol = char.Parse(splittedInput[1]);
+					List<int> indexes = new List<int>();
+
+					indexes = result
+						.Select((x, i) => i)
+						.Where(x => result[x] == symbol)
+						.ToList();
+
+					if (!indexes.Any())
+					{
+						Console.WriteLine("None");
+					}
+					else
+					{
+						Console.WriteLine(string.Join(" ", indexes));
+					}
+
+				}
+				else if (command == "Remove")
+				{
+					string searchedString = splittedInput[1];
+
+					while (result.Contains(searchedString))
+					{
+						int index = result.IndexOf(searchedString);
+						result = result.Remove(index, searchedString.Length);
+					}
+				}
+
+				input = Console.ReadLine();
+			}
 		}
 	}
 }
