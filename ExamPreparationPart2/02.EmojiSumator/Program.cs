eddfc0e948eb5605033f2b0137a842c127008ed5
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-06 17:25:02 +0300

    Add emoji-sumator

--- ExamPreparationPart2/02.EmojiSumator/Program.cs
+++ ExamPreparationPart2/02.EmojiSumator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _02.EmojiSumator
 {
@@ -6,7 +9,70 @@ namespace _02.EmojiSumator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<string> words = Console.ReadLine()
+				.Split(" ")
+				.Where(x => x.Contains(":"))
+				.ToList();
+
+			int[] asciiCodes = Console.ReadLine()
+				.Split(":")
+				.Select(int.Parse)
+				.ToArray();
+
+			string emojiCode = ":";
+
+			for (int i = 0; i < asciiCodes.Length; i++)
+			{
+				emojiCode += (char)asciiCodes[i];
+			}
+
+			emojiCode += ":";
+
+			string pattern = @"^(?<emoji>:(?<emojiValue>[a-z]{4,}):)$";
+			List<string> validEmojis = new List<string>();
+			int totalEmojiPower = 0;
+
+			foreach (var currentEmoji in words)
+			{
+				if (currentEmoji.Contains(",")
+					|| currentEmoji.Contains("!")
+					|| currentEmoji.Contains("?")
+					|| currentEmoji.Contains("."))
+				{
+					pattern = @"^(?<emoji>:(?<emojiValue>[a-z]{4,}):)(,|\.|!|\?a)$";
+				}
+
+				Match match = Regex.Match(currentEmoji, pattern);
+
+				if (match.Success)
+				{
+					string valueOfEmoji = match.Groups["emoji"].Value;
+
+					foreach (var symbol in valueOfEmoji)
+					{
+						if (symbol != ':')
+						{
+							totalEmojiPower += symbol;
+						}
+					}
+
+					validEmojis.Add(valueOfEmoji);
+				}
+
+				pattern = @"^(?<emoji>:(?<emojiValue>[a-z]{4,}):)$";
+			}
+
+			if (validEmojis.Contains(emojiCode))
+			{
+				totalEmojiPower *= 2;
+			}
+
+			if (validEmojis.Any())
+			{
+				Console.WriteLine($"Emojis found: {string.Join(", ", validEmojis)}");
+			}
+
+			Console.WriteLine($"Total Emoji Power: {totalEmojiPower}");
 		}
 	}
 }
