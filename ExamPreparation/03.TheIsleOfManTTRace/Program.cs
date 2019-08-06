78176d11ab178ecfacbf09efa918e3c41df1ff7d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-06 14:51:47 +0300

    Add the-isle-of-man-tt-race

--- ExamPreparation/03.TheIsleOfManTTRace/Program.cs
+++ ExamPreparation/03.TheIsleOfManTTRace/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace _03.TheIsleOfManTTRace
 {
@@ -6,7 +8,37 @@ namespace _03.TheIsleOfManTTRace
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			while (true)
+			{
+				string input = Console.ReadLine();
+				string lenghtPattern = @"=(\d+)!!";
+				Match m = Regex.Match(input, lenghtPattern);
+
+				if (m.Success)
+				{
+					int lenght = int.Parse(m.Groups[1].Value);
+					string pattern = $@"([#$%*&])(?<name>\w+)\1=(\d+)!!(?<text>.{{{lenght}}})$";
+					Match match = Regex.Match(input, pattern);
+
+					if (match.Success)
+					{
+						string name = match.Groups["name"].Value;
+						string coordinates = match.Groups["text"].Value;
+						StringBuilder text = new StringBuilder();
+
+						for (int i = 0; i < coordinates.Length; i++)
+						{
+							char symbol = (char)(coordinates[i] + lenght);
+							text.Append(symbol);
+						}
+
+						Console.WriteLine($"Coordinates found! {name} -> {text.ToString()}");
+						break;
+					}
+				}
+
+				Console.WriteLine("Nothing found!");
+			}
 		}
 	}
 }
